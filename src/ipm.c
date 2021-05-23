be2f41db5ee0292c3d6ee1589a220dec0d63b226
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2021-05-23 20:37:36 +0300

    Fix a memory leak
    Clean-up surrounding code a bit while at it
    
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/ipm.c
+++ src/ipm.c
@@ -18,15 +18,20 @@ GSList *window_priority_list = NULL;
 guint window_prio_max = 300;
 
 static gint
-window_priority_compare(window_priority_t *a, window_priority_t *b)
+window_priority_compare(gconstpointer _a, gconstpointer _b)
 {
+  const window_priority_t *a = _a;
+  const window_priority_t *b = _b;
+
   return a->priority != b->priority && a->widget != b->widget;
 }
 
 static gint
-window_priority_compare_priority(window_priority_t *a, window_priority_t *b)
+window_priority_compare_priority(gconstpointer _a, gconstpointer _b)
 {
   gint rv;
+  const window_priority_t *a = _a;
+  const window_priority_t *b = _b;
 
   if (a->priority >= b->priority)
     rv = a->priority != b->priority;
@@ -41,56 +46,56 @@ ipm_show_window(GtkWidget *widget, unsigned int priority)
 {
   window_priority_t *wp;
   window_priority_t data;
+  GdkDisplay *dpy;
+  gpointer layer;
 
   if (!widget || priority > window_prio_max)
-    return FALSE;
-
-  wp = (window_priority_t *)g_malloc(sizeof(window_priority_t));
-  if (!wp)
     return FALSE;
 
   data.priority = priority;
   data.widget = widget;
 
-  if (!g_slist_find_custom(window_priority_list, &data,
-                           (GCompareFunc)window_priority_compare))
+  if (g_slist_find_custom(window_priority_list, &data, window_priority_compare))
+    return FALSE;
+
+  wp = g_new(window_priority_t, 1);
+
+  if (!wp)
+    return FALSE;
+
+  dpy = gdk_display_get_default();
+
+  wp->priority = priority;
+  wp->widget = widget;
+
+  gtk_widget_realize(widget);
+
+  g_assert(app_ui_data->hsl_tab != NULL);
+
+  layer = g_hash_table_lookup(app_ui_data->hsl_tab, &priority);
+
+  if (layer)
   {
-    GdkDisplay *dpy = gdk_display_get_default();
-    gpointer layer;
+    Atom hsl_atom =
+        gdk_x11_get_xatom_by_name_for_display(dpy, "_HILDON_STACKING_LAYER");
 
-    wp->priority = priority;
-    wp->widget = widget;
-    gtk_widget_realize(widget);
-
-    g_assert(app_ui_data->hsl_tab != NULL);
-
-    layer = g_hash_table_lookup(app_ui_data->hsl_tab, &priority);
-    if (layer)
-    {
-      Atom hsl_atom =
-          gdk_x11_get_xatom_by_name_for_display(dpy,
-                                                "_HILDON_STACKING_LAYER");
-
-      XChangeProperty(gdk_x11_display_get_xdisplay(dpy),
-
+    XChangeProperty(gdk_x11_display_get_xdisplay(dpy),
 #ifdef WITH_GTK3
-                      gdk_x11_window_get_xid(gtk_widget_get_window(widget)),
-                      hsl_atom,
+                    gdk_x11_window_get_xid(gtk_widget_get_window(widget)),
+                    hsl_atom,
 #else
-                      gdk_x11_drawable_get_xid(widget->window), hsl_atom,
+                    gdk_x11_drawable_get_xid(widget->window), hsl_atom,
 #endif
-                      XA_CARDINAL, 32, PropModeReplace,
-                      (unsigned char *)layer, 1);
-    }
-
-    window_priority_list = g_slist_insert_sorted(window_priority_list, wp,
-                             (GCompareFunc)window_priority_compare_priority);
-    gtk_widget_show_all(widget);
-
-    return TRUE;
+                    XA_CARDINAL, 32, PropModeReplace,
+                    (unsigned char *)layer, 1);
   }
 
-  return FALSE;
+  window_priority_list = g_slist_insert_sorted(
+        window_priority_list, wp, window_priority_compare_priority);
+
+  gtk_widget_show_all(widget);
+
+  return TRUE;
 }
 
 gboolean
